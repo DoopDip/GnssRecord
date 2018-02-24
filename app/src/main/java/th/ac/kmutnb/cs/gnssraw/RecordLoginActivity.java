e9fe5799b54ee97df22bd65cbcfc30c829ed9ba3
DoopDip <narit.su@icloud.com>
2018-02-24 23:01:35 +0700

    Add RecordRegister

--- app/src/main/java/th/ac/kmutnb/cs/gnssraw/RecordLoginActivity.java
+++ app/src/main/java/th/ac/kmutnb/cs/gnssraw/RecordLoginActivity.java
@@ -1,13 +1,32 @@
 package th.ac.kmutnb.cs.gnssraw;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class RecordLoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "RecordLoginActivity";
+
+    private TextView textViewBtnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_login);
+
+        textViewBtnRegister = (TextView) findViewById(R.id.recordLogin_btnRegister);
+
+        textViewBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Click -> Register");
+                Intent intent = new Intent(RecordLoginActivity.this, RecordRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
